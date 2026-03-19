58e03362179ad31396eee5f0f091be672d4ee4c0
tounphylong-ux <tounphylong@gmail.com>
2026-03-20 02:06:43 +0700

    BLA-202

--- lib/ui/widgets/pickers/bla_location_picker.dart
+++ lib/ui/widgets/pickers/bla_location_picker.dart
@@ -1,6 +1,7 @@
-import 'package:blabla/services/location_service.dart';
+import 'package:blabla/data/repository/location/location_repository.dart';
 import 'package:blabla/ui/widgets/display/bla_divider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../model/ride/locations.dart';
 import '../../theme/theme.dart';
@@ -19,7 +20,7 @@ class BlaLocationPicker extends StatefulWidget {
 
 class _BlaLocationPickerState extends State<BlaLocationPicker> {
   String currentSearchText = "";
-
+  List<Location> _allLocation = [];
   void onTap(Location location) {
     Navigator.pop<Location>(context, location);
   }
@@ -31,13 +32,21 @@ class _BlaLocationPickerState extends State<BlaLocationPicker> {
   @override
   void initState() {
     super.initState();
-
+    _loadLocations();
     // Initilize the search bar if any initial location
     if (widget.initLocation != null) {
       setState(() {
         currentSearchText = widget.initLocation!.name;
       });
     }
+  }
+
+  Future<void> _loadLocations() async {
+    final repository = context.read<LocationRepository>();
+    final locations = await repository.getLocations();
+    setState(() {
+      _allLocation = locations;
+    });
   }
 
   void onSearchChanged(String search) {
@@ -50,7 +59,7 @@ class _BlaLocationPickerState extends State<BlaLocationPicker> {
     if (currentSearchText.length < 2) {
       return [];
     }
-    return LocationsService.availableLocations
+    return _allLocation
         .where(
           (location) => location.name.toUpperCase().contains(
             currentSearchText.toUpperCase(),
